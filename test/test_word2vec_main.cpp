0dcc50bf02aa89140226d61197c4ccea38e3a930
Joris Schellekens <joris.schellekens.1989@gmail.com>
2020-06-12 15:59:48 +0200

    more code in word2vec

--- test/test_word2vec_main.cpp
+++ test/test_word2vec_main.cpp
@@ -1,5 +1,16 @@
+
+#include "../neural_network.hpp"
 #include "../word2vec.hpp"
 
 int main()
 {
+    // sequence
+    std::vector<long> words = {1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 4, 7, 5, 6, 8, 7, 4, 5};
+
+    // empty neural network
+    auto nn = nn::init_neural_network({1000, 32, 1000});
+
+    // word2vec
+    word2vec::train(words, nn, 2);
+
 }
--- word2vec.hpp
+++ word2vec.hpp
@@ -2,7 +2,12 @@
 
 #include "matrix.hpp"
 
+#include <algorithm>
+#include <functional>
+#include <iostream>
 #include <map>
+#include <stdlib.h>
+#include <time.h>
 #include <tuple>
 #include <vector>
 
@@ -12,21 +17,64 @@ namespace word2vec
     std::vector<matrix::FloatMatrix> train(
         std::vector<long> sequence,
         std::vector<matrix::FloatMatrix> weights,
-        int window_size = 5)
+        int window_size = 5,
+        int number_of_negative_samples = 5)
     {
 
-        // frequency table
+        // frequency tuples
         std::map<long,long> word_frequency;
         for(int i=0; i<sequence.size(); i++)
         {
             ++word_frequency[sequence[i]];
         }
+        std::vector<std::pair<long,long>> word_frequency_tuples;
+        for(std::pair<long,long> e : word_frequency)
+        {
+            std::cout << e.first << " : " << e.second << std::endl;
+            word_frequency_tuples.push_back(e);
+        }
+
+        // total frequency
+        auto total_frequency = 0.0f;
+        for(int i=0; i<word_frequency_tuples.size(); i++)
+        {
+            total_frequency += word_frequency_tuples[i].second;
+        }
+        std::cout << "sum : " << total_frequency << std::endl;
+
+        // sort frequency table
+        auto sort_by_val = [](const std::pair<long,long>& t0, const std::pair<long,long>& t1)
+        {
+            return t0.second < t1.second;
+        };
+        std::sort(word_frequency_tuples.begin(), word_frequency_tuples.end(), sort_by_val);
+
+        // make 'select random word' function
+        std::vector<long> word_lookup_table;
+        auto k = word_frequency_tuples.size() - 1;
+        auto l = 100000;
+        while(word_lookup_table.size() < l)
+        {
+            auto n = (word_frequency_tuples[k].second / total_frequency) * l;
+            for(int i=0; i<n; i++)
+            {
+                word_lookup_table.push_back(word_frequency_tuples[k].first);
+            }
+            k--;
+        }
+        srand(time(NULL));
+        auto pick_random_word = [&word_lookup_table]()
+        {
+            return word_lookup_table[rand() % word_lookup_table.size()];
+        };
 
         // window view on sequence
-        for(int i=window_size; i<sequence.size() - window_size; i++)
+        for(int i=0; i<sequence.size() - window_size; i++)
         {
+
             // store samples
-            auto samples = std::vector<std::tuple<long,long,bool>>();
+            auto samples = std::vector<std::tuple<long, long, float>>();
+
             // positive samples in window
             for(int j=-window_size; j<window_size; j++)
             {
@@ -48,12 +96,26 @@ namespace word2vec
                 }
 
                 // add sample
-                samples.push_back(std::make_tuple(sequence[i], sequence[i+j], true));
+                samples.push_back(std::make_tuple(sequence[i], sequence[i+j], 1.0f));
             }
-            // negative samples
 
+            // negative samples
+            for(int j=0; j<number_of_negative_samples; j++)
+            {
+                samples.push_back(std::make_tuple(sequence[i], pick_random_word(), 0.0f));
+            }
+
+            /*
+             * training
+             */
+            for(int j=0; j<samples.size(); j++)
+            {
+                std::cout << std::get<0>(samples[j]) << "\t" << std::get<1>(samples[j]) << "\t" << std::get<2>(samples[j]) << std::endl;
+            }
         }
 
+        // return
+        return weights;
     }
 
 }
